78ea3b39032a33d1c0063aab90e779f2da387ce0
pythonstrup <pythonstrup@gmail.com>
2024-12-08 20:38:23 +0900

    PromptTemplate 초기화 방법의 종류

--- practice/bell/02_model_io/prompt.py
+++ practice/bell/02_model_io/prompt.py
@@ -2,9 +2,7 @@ from langchain import PromptTemplate
 
 prompt = PromptTemplate(
     template="{product}는 어느 회사에서 개발한 제품인가요?",
-    input_variables={
-        "product"
-    }
+    input_variables=["product"]
 )
 
 print(prompt.format(product="아이폰")) # 아이폰는 어느 회사에서 개발한 제품인가요?
--- practice/bell/02_model_io/prompt_and_language_model.py
+++ practice/bell/02_model_io/prompt_and_language_model.py
@@ -6,9 +6,7 @@ chat = ChatOpenAI(model="gpt-3.5-turbo")
 
 prompt = PromptTemplate(
     template="{product}는 어느 회사에서 개발한 제품인가요?",
-    input_variables={
-        "product"
-    }
+    input_variables=["product"]
 )
 
 result = chat([HumanMessage(content=prompt.format(product="아이폰"))])
