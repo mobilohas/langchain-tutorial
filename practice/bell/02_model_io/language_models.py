7f00aa536008365f469cfb81cba70a929a18aa55
pythonstrup <pythonstrup@gmail.com>
2024-12-08 20:03:49 +0900

    Language models 사용 예시

---
+++ practice/bell/02_model_io/language_models.py
@@ -0,0 +1,7 @@
+from langchain_community.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
+
+chat = ChatOpenAI(model="gpt-3.5-turbo")
+
+result = chat([HumanMessage(content="안녕하세요!")])
+print(result)
