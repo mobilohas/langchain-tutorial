5ff024c35db6cefeaf8ce38d7bbd7ecda90293b4
pythonstrup <pythonstrup@gmail.com>
2024-12-15 16:02:44 +0900

    검색 결과와 질문을 조합해 답변 생성

---
+++ practice/bell/03_retrieval/query_2.py
@@ -0,0 +1,46 @@
+from langchain_community.chat_models import ChatOpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain.schema import HumanMessage
+from langchain.vectorstores import Chroma
+
+embeddings = OpenAIEmbeddings(
+    model="text-embedding-ada-002"
+)
+
+database = Chroma(
+    persist_directory="./.data",
+    embedding_function=embeddings,
+)
+
+query = "비행 자동차의 최고 속도는?"
+documents = database.similarity_search(query)
+
+documents_string = ""
+
+for document in documents:
+    documents_string += f"""
+-------------------------------
+{document.page_content}
+"""
+
+prompt = PromptTemplate(
+    template="""문장을 바탕으로 질문에 답하세요.
+
+문장:
+{document}
+
+질문: {query}
+""",
+    input_variables=["document", "query"],
+)
+
+chat = ChatOpenAI(
+    model="gpt-3.5-turbo"
+)
+
+result = chat([
+    HumanMessage(content=prompt.format(document=documents_string, query=query)),
+])
+
+print(result.content)
